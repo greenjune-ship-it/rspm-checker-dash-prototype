be5abf09f057adf165f786bc87c73211cb507a67
Yulia_Iakovleva <Yulia_Iakovleva@epam.com>
2022-08-03 02:32:09 +0300

    ugly html state

--- app.py
+++ app.py
@@ -1,49 +1,71 @@
 import dash
 from dash import dcc
 from dash import html
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 
 app = dash.Dash(__name__)
 app.title = "RStudio Package Manager Checker"
 
 app.layout = html.Div(
-    id='app-container',
+    id='main_page',
     children=[
         html.Div(
-            id='header-area',
+            id='app-page-header',
             children=[
-                html.H1(
-                    id='header-title',
-                    children="RStudio Package Manager Checker"
-                ),
-                html.P(
-                    id='header-description',
-                    children="Application description"
+                html.H2(
+                    app.title
                 )
             ]
         ),
         html.Div(
-            id='menu-area',
+            id='app-page-content',
+            className='control-tabs',
             children=[
-                html.Div(
-                    className='menu-title',
-                    children='Menu Title'
-                ),
-                dcc.Dropdown(
-                    id='appname-filter',
-                    className='dropdown',
-                    options=["Default", "One", "Two", "Three"],
-                    clearable=False,
-                    value="Default"
-                )
+                html.Div(id='app-control-tabs', children=[
+                    dcc.Tabs(
+                        id='app-tabs',
+                        value='what-is',
+                        children=[
+                            dcc.Tab(
+                                label='About',
+                                value='what-is',
+                                children=html.Div(className='control-tab', children=[
+                                    html.H4(
+                                        className='what-is',
+                                        children='What is rspm-checker?'
+                                    ),
+                                    html.P('Some descriptive info about app')
+                                ])
+                            ),
+                            dcc.Tab(
+                                label='Inspect',
+                                value='select-app',
+                                children=html.Div(className='app-controls-block', children=[
+                                    dcc.Dropdown(
+                                        id='selected-app',
+                                        className='app-dropdown',
+                                        options=["Default", "One", "Two", "Three"],
+                                        clearable=False,
+                                        value="Default"
+                                    )
+                                ])
+                            )
+                        ]
+                    )
+                ])
             ]
         ),
-        html.Br(),
         html.Div(
-            id='logs-container',
+            id='app-container',
             children=[
                 html.Div(
-                   id="selected-value"
+                    id='logs-container',
+                    children=[
+                        html.Div(
+                            id="selected-value"
+                        )
+                    ]
                 )
             ]
         )
@@ -53,7 +75,7 @@ app.layout = html.Div(
 
 @app.callback(
     Output(component_id='selected-value', component_property='children'),
-    Input(component_id='appname-filter', component_property='value')
+    Input(component_id='selected-app', component_property='value')
 )
 def update_output_div(input_value):
     return f'Output: {input_value}'
